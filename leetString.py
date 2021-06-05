813184de3b10a55af6bf4a00b3a64bf218561918
Sota <watanabeso@crunchtimer.jp>
2021-06-06 02:39:22 +0900

    add:追加

--- leetString.py
+++ leetString.py
@@ -1,3 +1,4 @@
 input_line = input()
 print(input_line.replace('A', "4").replace('E', "3").replace('G', "6").replace(
     'I', "1").replace('O', "0").replace('S', "5").replace('Z', "2"))
+print("aaa")
