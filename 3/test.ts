e7653ee50e4b90305cda37d4e884be60d662d51a
watanabe-sota <819sota@gamail.com>
2021-06-24 01:38:26 +0900

    add:solution

---
+++ 3/test.ts
@@ -0,0 +1,11 @@
+const divisorSum = (num: number) => {
+  const results = []
+  for (let i = 1; i <= num; i++) {
+    if (num % i == 0 && i < 2000000) {
+      results.push(i)
+    }
+  }
+  return results.reduce(function (sum, element) {
+    return sum + element
+  }, 0)
+}
